204db5587f339d4fb2479c35ecca20120e846a41
Kirill Rasuli <kirill.rasuli@south.rt.ru>
2023-11-23 21:41:45 +0300

    Create and use modules

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"./greeting"
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/kvrasuli/hexlet-go/greeting"
 )
 
 func main() {
 	fmt.Println(greeting.Hello())
+
+	color.Red("We have red")
+	color.Magenta("And many others ..")
 }
